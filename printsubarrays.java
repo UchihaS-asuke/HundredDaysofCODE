893634879248d732d6966ab241ca59b6e75956f9
Sameer Bhardwaj <farfromweak413@gmail.com>
2023-10-10 19:44:51 +0530

    removed '=' from '<=' in the outer most loop

--- printsubarrays.java
+++ printsubarrays.java
@@ -1,7 +1,7 @@
 public class printsubarrays {
     public static void prntsbarry(int[] arry){
         int l=arry.length;
-        for(int i=0;i<=l;i++){
+        for(int i=0;i<l;i++){
             for(int j=i+1;j<l;j++){
                 for(int k=i;k<=j;k++){
                     System.out.print(arry[k]+" ");
